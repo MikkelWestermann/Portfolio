14cf81026f56588de768c7b7190ddc9c38e6caf4
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-03 22:13:06 +0200

    Add basic landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from './components/landing';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Route exact path='/' component={Landing} /> 
+      </Router>
     </div>
   );
 }
