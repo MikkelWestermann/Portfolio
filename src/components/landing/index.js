43e69a9fb6f812b613069edeb6162084e0ab8084
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-10 12:03:20 +0200

    Change "white" to #eee Use ++ instead of += 1 in for loops

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -53,7 +53,7 @@ class Landing extends Component {
       p.setup = () => {
         p.createCanvas(p.windowWidth < maxWindowWidth ? p.windowWidth : maxWindowWidth, p.windowHeight);
         p.noStroke();
-        colorA = p.color(255,255,255);
+        colorA = p.color(238);
         colorB = p.color(0, 150, 136);
       }
   
@@ -61,14 +61,14 @@ class Landing extends Component {
         p.background(24, 40, 54);
         phase = p.frameCount * speed;
         
-        for(let strand = 0; strand < numStrands; strand += 1) {
+        for(let strand = 0; strand < numStrands; strand++) {
           let strandPhase = phase + p.map(strand, 0, numStrands, 0, p.TWO_PI);
           
-          for(let col = 0; col < numCols; col += 1) {
+          for(let col = 0; col < numCols; col++) {
             const colOffset = p.map(col, 0, numCols, 0, p.TWO_PI);
             const x = p.map(col, 0, numCols, 50, p.width - 50);
             
-            for(let row = 0; row < numRows; row += 1) {
+            for(let row = 0; row < numRows; row++) {
               const y = p.height/2 + row * 10 + p.sin(strandPhase + colOffset) * 50;
               const sizeOffset = (p.cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
               const circleSize = sizeOffset * maxCircleSize;
