0fa9d5e11bde8251cb3a53d3b658663e2e4f7da9
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-10 12:35:57 +0200

    Add landing anim actions Add landing anim action style

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'; 
 
+import Button from 'antd/lib/button';
+
 import { 
   watchViewport, 
   unwatchViewport
@@ -43,6 +45,12 @@ class Landing extends Component {
     }
   }
 
+  pauseAnim = () => {
+    this.setState(prevState =>  (
+      { pauseAnim: !prevState.pauseAnim } 
+    ))
+  }
+
   sketch = p => {
     // Not my artwork -> https://blog.kadenze.com/creative-technology/p5-js-crash-course-recreate-art-you-love/
     const maxCircleSize = 40, numRows = 10, numCols = 50, numStrands = 2; 
@@ -58,23 +66,25 @@ class Landing extends Component {
       }
   
       p.draw = () => {
-        p.background(24, 40, 54);
-        phase = p.frameCount * speed;
-        
-        for(let strand = 0; strand < numStrands; strand++) {
-          let strandPhase = phase + p.map(strand, 0, numStrands, 0, p.TWO_PI);
+        if (!this.state.pauseAnim) {
+          p.background(24, 40, 54);
+          phase = p.frameCount * speed;
           
-          for(let col = 0; col < numCols; col++) {
-            const colOffset = p.map(col, 0, numCols, 0, p.TWO_PI);
-            const x = p.map(col, 0, numCols, 50, p.width - 50);
+          for(let strand = 0; strand < numStrands; strand++) {
+            let strandPhase = phase + p.map(strand, 0, numStrands, 0, p.TWO_PI);
             
-            for(let row = 0; row < numRows; row++) {
-              const y = p.height/2 + row * 10 + p.sin(strandPhase + colOffset) * 50;
-              const sizeOffset = (p.cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
-              const circleSize = sizeOffset * maxCircleSize;
+            for(let col = 0; col < numCols; col++) {
+              const colOffset = p.map(col, 0, numCols, 0, p.TWO_PI);
+              const x = p.map(col, 0, numCols, 50, p.width - 50);
               
-              p.fill(p.lerpColor(colorA, colorB, row / numRows));
-              p.ellipse(x, y, circleSize, circleSize);
+              for(let row = 0; row < numRows; row++) {
+                const y = p.height/2 + row * 10 + p.sin(strandPhase + colOffset) * 50;
+                const sizeOffset = (p.cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
+                const circleSize = sizeOffset * maxCircleSize;
+                
+                p.fill(p.lerpColor(colorA, colorB, row / numRows));
+                p.ellipse(x, y, circleSize, circleSize);
+              }
             }
           }
         }
@@ -85,12 +95,24 @@ class Landing extends Component {
       }
   }
   render() {
+    const { pauseAnim } = this.state;
     return (
       <div id='landing-anim'>
         <P5Wrapper sketch={this.sketch}></P5Wrapper>
         <div id='landing-anim-me'>
           <h1>Hi, I'm <span>Mikkel Westermann</span></h1>
           <h2>I'm a full-stack developer</h2>
+        </div>
+        <div id='landing-anim-actions'>
+          <div>
+            <Button>Download My Resume</Button>
+            <Button>View My Work -></Button>
+          </div>
+          {
+            !pauseAnim ?
+            <p>It's a pretty taxing animation running in the background click <span onClick={this.pauseAnim}>Here</span> to pause it</p>:
+            <p>It's a pretty awesome animation running in the background click <span onClick={this.pauseAnim}>Here</span> to unpause it</p>
+          }
         </div>
       </div>
     )
