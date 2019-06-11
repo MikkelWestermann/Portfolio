f7f7577422909b9b3600901cb3fc583a45cbdaa4
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-11 20:31:04 +0200

    Add skills section component

---
+++ src/components/skills/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useLaxElement } from 'use-lax';
+
+import Section from '../Section';
+
+// import './styles.scss'
+
+const Skills = () => {
+  // const meImage = useLaxElement();
+  return (
+    <Section title='Skills' id='skills-section'>
+      
+    </Section>
+  );
+}
+
+export default Skills
