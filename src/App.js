b5b68a6cd0238d6cb2d2ca0ea38103996ba0c035
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-11 20:29:24 +0200

    Import and init uselax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from './components/landing';
+import { useLax } from 'use-lax';
 import './App.scss';
 
 function App() {
+
+  useLax()
+
   return (
     <div className="App">
       <Router>
