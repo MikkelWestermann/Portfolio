78478ffd70c2bef7087e416bb5e75a639cdda40a
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-12 21:46:42 +0200

    Add code skill display

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -3,13 +3,28 @@ import { useLaxElement } from 'use-lax';
 
 import Section from '../Section';
 
+import { Divider } from 'antd';
+
 import './styles.scss'
 
 const Skills = () => {
-  // const meImage = useLaxElement();
+  const javascriptImage = useLaxElement();
+  const cSharpImage = useLaxElement();
+  const pythonImage = useLaxElement();
   return (
     <Section title='Skills' id='skills-section'>
-      
+      <div className='skills-code-wrapper'>
+        <div ref={javascriptImage} data-lax-preset='fadeIn leftToRight-.05' className='skills-code-image' />
+      </div>
+      <Divider orientation="left">JavaScript</Divider>
+      <div className='skills-code-wrapper skills-code-right'>
+        <div ref={cSharpImage} data-lax-preset='fadeIn rightToLeft-.05' className='skills-code-image' />
+      </div>
+      <Divider orientation="right">C#</Divider>
+      <div className='skills-code-wrapper'>
+        <div ref={pythonImage} data-lax-preset='fadeIn leftToRight-.05' className='skills-code-image' />
+      </div>
+      <Divider orientation="left">Python</Divider>
     </Section>
   );
 }
