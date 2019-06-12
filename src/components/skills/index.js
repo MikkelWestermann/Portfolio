511f6f6f52c4785842f9bafe6eb5013e7cb1ccd0
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-12 21:57:18 +0200

    Remove sliding of code images

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -14,15 +14,15 @@ const Skills = () => {
   return (
     <Section title='Skills' id='skills-section'>
       <div className='skills-code-wrapper'>
-        <div ref={javascriptImage} data-lax-preset='fadeIn leftToRight-.05' className='skills-code-image' />
+        <div ref={javascriptImage} data-lax-preset='fadeIn' className='skills-code-image' />
       </div>
       <Divider orientation="left">JavaScript</Divider>
       <div className='skills-code-wrapper skills-code-right'>
-        <div ref={cSharpImage} data-lax-preset='fadeIn rightToLeft-.05' className='skills-code-image' />
+        <div ref={cSharpImage} data-lax-preset='fadeIn' className='skills-code-image' />
       </div>
       <Divider orientation="right">C#</Divider>
       <div className='skills-code-wrapper'>
-        <div ref={pythonImage} data-lax-preset='fadeIn leftToRight-.05' className='skills-code-image' />
+        <div ref={pythonImage} data-lax-preset='fadeIn' className='skills-code-image' />
       </div>
       <Divider orientation="left">Python</Divider>
     </Section>
