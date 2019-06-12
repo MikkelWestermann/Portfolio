419c8a5d01cd84ca47c93b741048ec6f94673b43
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-12 22:10:02 +0200

    Add a bit of text

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -3,7 +3,7 @@ import { useLaxElement } from 'use-lax';
 
 import Section from '../Section';
 
-import { Divider } from 'antd';
+import { Divider, Typography } from 'antd';
 
 import './styles.scss'
 
@@ -11,6 +11,7 @@ const Skills = () => {
   const javascriptImage = useLaxElement();
   const cSharpImage = useLaxElement();
   const pythonImage = useLaxElement();
+  const { Title, Paragraph } = Typography;
   return (
     <Section title='Skills' id='skills-section'>
       <div className='skills-code-wrapper'>
@@ -25,6 +26,12 @@ const Skills = () => {
         <div ref={pythonImage} data-lax-preset='fadeIn' className='skills-code-image' />
       </div>
       <Divider orientation="left">Python</Divider>
+      <Title level={3}>Other Tecknologies</Title>
+      <Paragraph>
+        Through my work I have had the opportunity to work with a <strong>lot</strong> of really cool tecknologies. 
+        Because I work at Hairpal I have a lot of different roles, which enables me to explore different tools and tecknologies, that i wouldn't necessarily have worked with at this stage of my career. 
+        At Hairpal I have worked with full-stack web development (obviously): NodeJs, React, MongoDB, Postgres, GraphQL, etc., deployment: Kubernetes, Docker and Bitbucket Pipelines, Task automation: Python Scheduling, Flask, Sendgrid
+      </Paragraph>
     </Section>
   );
 }
