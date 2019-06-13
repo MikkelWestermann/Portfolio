85c72da1e26c1b04e833cd552ccd62137922138e
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-13 20:39:15 +0200

    Add navbar to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Landing from './components/landing';
+import Navbar from './components/Navbar';
 import Footer from './components/footer';
 import { useLax } from 'use-lax';
 import './App.scss';
+
+// Routes
+import Landing from './components/landing';
+
 
 function App() {
 
@@ -11,6 +15,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Router>
         <Route exact path='/' component={Landing} /> 
       </Router>
