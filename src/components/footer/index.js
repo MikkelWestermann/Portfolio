6c4692a6ea6013dbf882bd0e7022b86c85786a83
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-13 21:10:25 +0200

    Add footer content

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Container from '../Container';
 
-import { Typography } from 'antd'
+import { Typography, Icon } from 'antd'
 
 import './styles.scss';
 
@@ -13,7 +13,28 @@ const Footer = props => {
       <div id='footer-bubble' />
       <div id='small-footer-bubble' />
       <Container>
-        <Title style={{color: '#eee'}} level={4}>Footer</Title>
+        <Title style={{color: '#eee'}} level={3}>Footer</Title>
+        <div id='footer-content'>
+          <div>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+            <p>Other content</p>
+          </div>
+          <div id='footer-social'>
+            <Title level={4}>Social</Title>
+            <div id='footer-social-buttons'>
+              <Icon style={{ fontSize: '2.5em' }} type="github" />
+              <Icon style={{ fontSize: '2.5em' }} type="linkedin" theme="filled" />
+              <Icon style={{ fontSize: '2.5em' }} type="twitter" />
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   )
