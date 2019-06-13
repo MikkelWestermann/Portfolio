91aaa5502e3eeaa80f1fda896d5f508ecc2887df
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-13 20:45:38 +0200

    Move header and footer inside Rotuer to have access to Link Add react router switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
 import { useLax } from 'use-lax';
@@ -15,11 +15,13 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
       <Router>
-        <Route exact path='/' component={Landing} /> 
+        <Navbar />
+          <Switch>
+            <Route exact path='/' component={Landing} /> 
+          </Switch>
+        <Footer />
       </Router>
-      <Footer />
     </div>
   );
 }
