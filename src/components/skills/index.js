9d55a4b9eb1a24703eda0b0834579e425afa2826
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-15 21:52:40 +0200

    Fix text in other technologies in skills section

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -11,7 +11,7 @@ const Skills = () => {
   const javascriptImage = useLaxElement();
   const cSharpImage = useLaxElement();
   const pythonImage = useLaxElement();
-  const { Title, Paragraph } = Typography;
+  const { Title, Paragraph, Text } = Typography;
   return (
     <Section title='Skills' id='skills-section'>
       <div className='skills-code-wrapper'>
@@ -26,12 +26,44 @@ const Skills = () => {
         <div ref={pythonImage} data-lax-preset='fadeIn' className='skills-code-image' />
       </div>
       <Divider orientation="left">Python</Divider>
-      <Title level={3}>Other Tecknologies</Title>
-      <Paragraph>
-        Through my work I have had the opportunity to work with a <strong>lot</strong> of really cool tecknologies. 
-        Because I work at Hairpal I have a lot of different roles, which enables me to explore different tools and tecknologies, that i wouldn't necessarily have worked with at this stage of my career. 
-        At Hairpal I have worked with full-stack web development (obviously): NodeJs, React, MongoDB, Postgres, GraphQL, etc., deployment: Kubernetes, Docker and Bitbucket Pipelines, Task automation: Python Scheduling, Flask, Sendgrid
-      </Paragraph>
+      <Typography>
+        <Title level={3}>Other Technologies</Title>
+        <Paragraph>
+          Through my work I have had the opportunity to work with a <strong>lot</strong> of really cool technologies. 
+          Because I work at Hairpal I have a lot of different roles, which enables me to explore different tools and technologies, that i wouldn't necessarily have worked with at this stage of my career. 
+          During my time at Hairpal, I have worked with: 
+        </Paragraph>
+        <Paragraph>
+          <ul>
+            <li>
+              <Text strong>Full-stack web development</Text> Here I have worked with a long list of technologies, including: 
+              <ul>
+                <li>Node.js</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>SQL, NoSQL, and GraphQL</li>
+              </ul>
+            </li>
+            <li>
+              <Text strong>Dev Ops</Text> We have a really modern setup, with technologies including: 
+              <ul>
+                <li>Docker</li>
+                <li>Kubernetes</li>
+                <li>Bitbucket Pipelines</li>
+              </ul>
+            </li>
+            <li>
+              <Text strong>Task automation</Text> Automating all the boring stuff with: 
+              <ul>
+                <li>Python / Flask</li>
+                <li>Sendgrid</li>
+                <li>PDFKit</li>
+              </ul>
+            </li>
+          </ul>
+        </Paragraph>
+
+      </Typography>
     </Section>
   );
 }
