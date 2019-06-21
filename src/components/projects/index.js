11adbab992d2c308b3b8fcfed2dd5f040492f3e4
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-21 21:33:31 +0200

    Add basic project component and styles

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,10 +1,27 @@
 import React from 'react'; 
+import { Typography } from 'antd';
+import Container from '../Container';
+
+import './styles.scss';
+
+const Project = props => {
+  return (
+    <div className='project'>
+      <div className='project-background'>
+        <div className='project-icon' />
+      </div>
+      <div>Project Info</div>
+    </div>
+  )
+}
 
 const Projects = props => {
+  const { Title } = Typography;
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <Container>
+      <Title level={1} className='page-header'>Projects</Title>
+      <Project />
+    </Container>
   )
 }
 
