b4d26d25d3842005ac6da199cb679b7341dcb6f2
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-30 14:11:42 +0200

    Wrap text in container

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLaxElement } from 'use-lax';
 
 import Section from '../Section';
+import Container from '../Container';
 
 import { Typography, Icon } from 'antd';
 
@@ -9,7 +10,7 @@ import './styles.scss'
 
 const About = () => {
   const meArrow = useLaxElement()
-  const { Paragraph, Text } = Typography;
+  const { Paragraph, Text, Title } = Typography;
   return (
     <Section title='About' id='about-section'>
       <div id='about-me-image'>
@@ -20,11 +21,17 @@ const About = () => {
         </div>
         <div id='me-arrow' ref={meArrow} data-lax-preset='fadeIn rightToLeft-.2'><Icon type="arrow-left" /> That's me</div>
       </div>
-      <Typography>
-        <Paragraph>
-          <Text strong>Hi! And welcome to my portfolio / playground.</Text> This is a website for showcasing my work, and for me to try out awesome new (or new to me) technologies like serverless architecture and React Hooks.
-        </Paragraph>
-      </Typography>
+      <Container text>
+        <Typography>
+          <Paragraph>
+            <Text strong>Hi! And welcome to my portfolio / playground.</Text> This is a website for showcasing my work, and for me to try out awesome new (or new to me) technologies like serverless architecture and React Hooks.
+          </Paragraph>
+
+          <Title level={3}>Tech Stack</Title>
+          
+
+        </Typography>
+      </Container>
     </Section>
   );
 }
