8f204b7addb2efe5e9db7cff4ad819e70d121ecb
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-30 16:59:32 +0200

    Move tornis scroll watch to app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Footer from './components/footer';
 import { useLax } from 'use-lax';
 import './App.scss';
 
+import { 
+  watchViewport, 
+  unwatchViewport
+} from 'tornis';
+
 // Routes
 import Landing from './components/landing';
 import Projects from './components/projects';
 
 
-function App() {
-
+const App = () => {
   useLax()
+
+  const [scroll, setScroll] = useState();
+  
+  useEffect(() => {
+    watchViewport(tornis)
+
+    return () => {
+      unwatchViewport(tornis)
+    }
+  }, [])
+
+  const tornis = ({ scroll }) => {
+    if (scroll.changed) {
+      setScroll(scroll)
+    }
+  }
+
 
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar scroll={scroll} />
           <Switch>
             <Route exact path='/projects' component={Projects} />
             <Route exact path='/' component={Landing} /> 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,30 +3,10 @@ import Menu from './menu'
 import { Drawer, Button, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 
-import { 
-  watchViewport, 
-  unwatchViewport
-} from 'tornis';
-
 import './styles.scss';
 
-const Navbar = () => {
+const Navbar = ({ scroll }) => {
   const [visible, setVisible] = useState(false);
-  const [scroll, setScroll] = useState();
-  
-  useEffect(() => {
-    watchViewport(tornis)
-
-    return () => {
-      unwatchViewport(tornis)
-    }
-  }, [])
-
-  const tornis = ({ scroll }) => {
-    if (scroll.changed) {
-      setScroll(scroll)
-    }
-  }
 
   const showDrawer = () => {
     setVisible(true);
@@ -35,29 +15,29 @@ const Navbar = () => {
     setVisible(false);
   };
 
-    return (
-        <nav className={`menuBar ${scroll && scroll.top > 50 ? 'scrolled-menubar' : ''}`}>
-          <div className="logo">
-            <Link to='/'><span>{'<Home />'}</span></Link>
+  return (
+      <nav className={`menuBar ${scroll && scroll.top > 50 ? 'scrolled-menubar' : ''}`}>
+        <div className="logo">
+          <Link to='/'><span>{'<Home />'}</span></Link>
+        </div>
+        <div className="menuCon">
+          <div className="rightMenu">
+              <Menu />
           </div>
-          <div className="menuCon">
-            <div className="rightMenu">
-                <Menu />
-            </div>
-            <Button className="barsMenu" type="primary" onClick={showDrawer}>
-              <Icon type="menu" />
-            </Button>
-            <Drawer
-              title="Basic Drawer"
-              placement="right"
-              closable={false}
-              onClose={onClose}
-              visible={visible}
-            >
-              <Menu sidebar={true} />
-            </Drawer>
-          </div>
-        </nav>
+          <Button className="barsMenu" type="primary" onClick={showDrawer}>
+            <Icon type="menu" />
+          </Button>
+          <Drawer
+            title="Basic Drawer"
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Menu sidebar={true} />
+          </Drawer>
+        </div>
+      </nav>
     );
 }
 export default Navbar;
