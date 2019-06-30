cbd2343a1c6a5966b13e0f7254663623280342d7
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-06-30 16:47:47 +0200

    Fix navbar hover effects

--- src/components/Navbar/menu.js
+++ src/components/Navbar/menu.js
@@ -9,17 +9,20 @@ const MenuItemGroup = Menu.ItemGroup;
 const MenuComponent = ({ sidebar }) => {
   return (
     <Menu mode={sidebar ? 'vertical' : 'horizontal'}>
-      <Menu.Item key="mail">
+      <Menu.Item key="projects">
         <Link to='/projects'>Projects</Link>
+        <div className='nav-background-circle' />
       </Menu.Item>
-      <SubMenu title={<span>Demos</span>}>
+      <SubMenu title={<div><span>Demos</span><div className='nav-background-circle' /></div>}>
         <MenuItemGroup title="Backend">
           <Menu.Item key="url-shortener"><Link to='/demos/url-shortener'>Url Shortener</Link></Menu.Item>
           <Menu.Item key="email-scheduler"><Link to='/demos/email-scheduler'>Email Scheduler</Link></Menu.Item>
         </MenuItemGroup>
+        <div className='nav-background-circle' />
       </SubMenu>
-      <Menu.Item key="alipay">
+      <Menu.Item key="contact">
         <a href='/#contact'>Contact</a>
+        <div className='nav-background-circle' />
       </Menu.Item>
     </Menu>
   );
