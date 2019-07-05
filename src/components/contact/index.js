ee69081dbe857bb67b9e290061f8f290b223bbf1
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-07-05 21:53:28 +0200

    Add form to contact section

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Form, Button } from 'antd';
 // import { useLaxElement } from 'use-lax';
 
 import Section from '../Section';
 
-// import './styles.scss'
+import './styles.scss'
 
 const Contact = () => {
   // const meImage = useLaxElement();
   return (
     <Section title='Contact' id='contact'>
-      
+      <Form className='contact-form'>
+        <Button size='large' type="primary" htmlType="submit" className="contact-form-button">
+          Submit
+        </Button>
+      </Form>
     </Section>
   );
 }
