2687fd193c7a62b733a29f1def3fd7da3beb3184
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-07-06 15:58:39 +0200

    Add messages for success and failed contact submission

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -15,12 +15,44 @@ const Contact = ( { form } ) => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    const now = new Date()
+    const scheduledDate = new Date(now.getTime() + 5*60000)
     form.validateFields((err, values) => {
       if (!err) {
         setFormSubmitState({ isFetching: true })
+        
+        fetch('http://localhost:5555/', {
+          method: 'POST',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify({
+            email: 'mikkel.westermann.98@gmail.com',
+            date: scheduledDate,
+            data: {
+              subject: `email from ${values.name} about ${values.about}`
+            }
+          })
+        })
+          .then(res => setFormSubmitState({ isFetching: false, status: res.status }))
       }
     });
   };
+
+  const submitButtonIcon = () => {
+    if (formSubmitState) {
+      if (formSubmitState.isFetching) {
+        return <Icon type="loading" />
+      } else if (formSubmitState.status < 400) {
+        return <Icon type="check" />
+      } else {
+        return <Icon type="exclamation" />
+      }
+    } else {
+      return <Icon type="upload" />
+    }
+  }
 
   return (
     <Section title='Contact' id='contact'>
@@ -74,9 +106,11 @@ const Contact = ( { form } ) => {
         </Form.Item>
         <Form.Item>
           <Button size='large' type="primary" htmlType="submit" className="contact-form-button custom-button-hover">
-            Submit { formSubmitState && formSubmitState.isFetching ? <Icon type="loading" /> : <Icon type="upload" />}
+            Submit { submitButtonIcon() }
           </Button>
         </Form.Item>
+        <p className={`contact-submission-confirmation ${formSubmitState && formSubmitState.status < 400 ? 'contact-submission-confirmation-show' : ''}`}>You will receive an email when I have received your message to confirm your submission</p>
+        <p className={`contact-submission-confirmation ${formSubmitState && formSubmitState.status > 400 ? 'contact-submission-confirmation-fail' : ''}`}>Something went wrong... Are you sure you have filled the form correctly? Otherwise contact me <a href='mailto:mikkel.westermann.98@gmail.com'>here</a></p>
       </Form>
     </Section>
   );
