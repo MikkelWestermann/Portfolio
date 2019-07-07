c5817317e7667a196c2b52a057774c5b38390f60
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-07-07 20:44:02 +0200

    Add lax anim to subpage header

--- src/components/sub-page/index.js
+++ src/components/sub-page/index.js
@@ -1,20 +1,25 @@
 import React from 'react'; 
 import { Typography } from 'antd';
 import Container from '../Container';
+import { useLaxElement } from 'use-lax';
 
 import './styles.scss';
 
-const SubPage = ({ children, title }) => (
-  <div className='sub-page-wrapper'>
-    <div className='page-header'>
-      <Typography.Title level={1}>{ title }</Typography.Title>
-      <div id='header-large-bubble' />
-      <div id='header-small-bubble' />
+const SubPage = ({ children, title }) => {
+  const smallBubble = useLaxElement()
+  const largeBubble = useLaxElement()
+  return (
+    <div className='sub-page-wrapper'>
+      <div className='page-header'>
+        <Typography.Title level={1}>{ title }</Typography.Title>
+        <div ref={largeBubble} id='header-large-bubble' data-lax-preset="rightToLeft-.25" />
+        <div ref={smallBubble} id='header-small-bubble' data-lax-preset="leftToRight-.25" />
+      </div>
+      <Container>
+        { children }
+      </Container>
     </div>
-    <Container>
-      { children }
-    </Container>
-  </div>
-)
+  )
+}
 
 export default SubPage
