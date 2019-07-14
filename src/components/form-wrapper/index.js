ac8263fbb1a2d0540e8f9d4e26ab76ae4dd50eda
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-07-14 12:13:20 +0200

    Add form wrapper component

---
+++ src/components/form-wrapper/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from '../Container';
+
+import { useLaxElement } from 'use-lax';
+
+import './styles.scss';
+
+const FormWrapper = ({ children }) => {
+  const largeContactBubble = useLaxElement()
+  const smallContactBubble = useLaxElement()
+  return (
+    <Container className='form-wrapper'>
+      <div className='contact-form-bubble-large' ref={largeContactBubble} data-lax-preset="leftToRight-.1" />
+      <div className='contact-form-bubble-small' ref={smallContactBubble} data-lax-preset="rightToLeft-.15" />
+      { children }
+    </Container>
+  )
+}
+
+export default FormWrapper;
