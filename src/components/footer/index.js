edfa037a18950d4612c0d673b2ec9922d6589016
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-07-15 21:03:55 +0200

    Add copyright

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -32,6 +32,7 @@ const Footer = props => {
             </div>
           </div>
         </div>
+        <p className='copyright'>Mikkel Theiss Westermann &copy; {new Date().getFullYear()}</p>
       </Container>
     </div>
   )
