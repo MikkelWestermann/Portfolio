7415b72b7772f8ede6031b6ea80a859092c2ef05
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-07-22 20:22:11 +0200

    Add ability to add inline styles to form wrapper component

--- src/components/form-wrapper/index.js
+++ src/components/form-wrapper/index.js
@@ -5,11 +5,11 @@ import { useLaxElement } from 'use-lax';
 
 import './styles.scss';
 
-const FormWrapper = ({ children }) => {
+const FormWrapper = ({ children, style }) => {
   const largeContactBubble = useLaxElement()
   const smallContactBubble = useLaxElement()
   return (
-    <Container className='form-wrapper'>
+    <Container className='form-wrapper' style={style}>
       <div className='contact-form-bubble-large' ref={largeContactBubble} data-lax-preset="leftToRight-.1" />
       <div className='contact-form-bubble-small' ref={smallContactBubble} data-lax-preset="rightToLeft-.15" />
       { children }
