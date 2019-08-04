50b31e33555de199938b5a70e16c2b32ffcc4ee2
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-08-04 15:49:10 +0200

    Fix demos hover effect

--- src/components/Navbar/menu.js
+++ src/components/Navbar/menu.js
@@ -14,9 +14,9 @@ const MenuComponent = ({ sidebar, onClose }) => {
         <div className='nav-background-circle' />
       </Menu.Item>
       <SubMenu title={<div><span>Demos</span><div className='nav-background-circle' /></div>}>
-        <MenuItemGroup title="Backend">
-          <Menu.Item key="url-shortener"><Link onClick={() => { window.scrollTo(0,0); if (onClose) {onClose()} }} to='/demos/url-shortener'>Url Shortener</Link></Menu.Item>
-          <Menu.Item key="email-scheduler"><Link onClick={() => { window.scrollTo(0,0); if (onClose) {onClose()} }} to='/demos/email-scheduler'>Email Scheduler</Link></Menu.Item>
+        <MenuItemGroup title="Backend" className='demos'>
+          <Menu.Item key="url-shortener"><Link className='demo-link' onClick={() => { window.scrollTo(0,0); if (onClose) {onClose()} }} to='/demos/url-shortener'>Url Shortener</Link></Menu.Item>
+          <Menu.Item key="email-scheduler"><Link className='demo-link' onClick={() => { window.scrollTo(0,0); if (onClose) {onClose()} }} to='/demos/email-scheduler'>Email Scheduler</Link></Menu.Item>
         </MenuItemGroup>
         <div className='nav-background-circle' />
       </SubMenu>
