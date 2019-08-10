ff9e771ca59796184ce807badcd213f76bd466fa
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2019-08-10 11:51:57 +0200

    Fix a bit of text in the skills component

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 import Section from '../Section';
+import Container from '../Container';
 
 import { Divider, Typography } from 'antd';
 
@@ -39,44 +40,44 @@ const Skills = () => {
         </div>
       </div>
       <Divider orientation="left">Python</Divider>
-      <Typography>
-        <Title level={3}>Other Technologies</Title>
-        <Paragraph>
-          Through my work I have had the opportunity to work with a <strong>lot</strong> of really cool technologies. 
-          Because I work at Hairpal I have a lot of different roles, which enables me to explore different tools and technologies, that i wouldn't necessarily have worked with at this stage of my career. 
-          During my time at Hairpal, I have worked with: 
-        </Paragraph>
-        <Paragraph>
-          <ul>
-            <li>
-              <Text strong>Full-stack web development</Text> Here I have worked with a long list of technologies, including: 
-              <ul>
-                <li>Node.js</li>
-                <li>React</li>
-                <li>Redux</li>
-                <li>SQL, NoSQL, and GraphQL</li>
-              </ul>
-            </li>
-            <li>
-              <Text strong>Dev Ops</Text> We have a really modern setup, with technologies including: 
-              <ul>
-                <li>Docker</li>
-                <li>Kubernetes</li>
-                <li>Bitbucket Pipelines</li>
-              </ul>
-            </li>
-            <li>
-              <Text strong>Task automation</Text> Automating all the boring stuff with: 
-              <ul>
-                <li>Python / Flask</li>
-                <li>Sendgrid</li>
-                <li>PDFKit</li>
-              </ul>
-            </li>
-          </ul>
-        </Paragraph>
-
-      </Typography>
+      <Container text>
+        <Typography>
+          <Title level={3}>Other Technologies</Title>
+          <Paragraph>
+            Through my work I have had the opportunity to work with a <strong>lot</strong> of really cool technologies. 
+            Because I work at a smaller startup I have a lot of different roles, which enables me to explore different tools and technologies, that i wouldn't necessarily have worked with at this stage of my career. 
+            I have worked with: 
+          </Paragraph>
+          <Paragraph>
+            <ul>
+              <li>
+                <Text strong>Full-stack web development</Text> Here I have worked with a long list of technologies, including: 
+                <ul>
+                  <li>Node.js</li>
+                  <li>React</li>
+                  <li>Redux</li>
+                  <li>SQL, NoSQL, and GraphQL</li>
+                </ul>
+              </li>
+              <li>
+                <Text strong>Dev Ops</Text> We have a really modern setup, with technologies including: 
+                <ul>
+                  <li>Docker</li>
+                  <li>Kubernetes</li>
+                  <li>Bitbucket Pipelines</li>
+                </ul>
+              </li>
+              <li>
+                <Text strong>Task automation</Text> Automating all the boring stuff with: 
+                <ul>
+                  <li>Python / Flask</li>
+                  <li>Sendgrid</li>
+                </ul>
+              </li>
+            </ul>
+          </Paragraph>
+        </Typography>
+      </Container>
     </Section>
   );
 }
